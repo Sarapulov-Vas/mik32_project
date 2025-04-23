1dc44313ed956b1a360345da6440435c91bb9fcb
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-04-23 23:50:12 +0300

    feat: update the radar module
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

--- app/main.c
+++ app/main.c
@@ -13,17 +13,17 @@ int main()
     radar.HC_SR04.Echo_Pin = GPIO_PIN_1;
 
     radar.spi.Instance = SPI_1;
-    radar.spi.Init.ManualCS = SPI_MANUALCS_ON;
-    radar.spi.Init.ChipSelect = SPI_CS_0;
 
     radar.scr.Init.Interface = HAL_SPI;
     radar.scr.Init.Spi = &radar.spi;
     radar.scr.Init.SSD1306_DC_Port = GPIO_1;
     radar.scr.Init.SSD1306_DC_Pin = GPIO_PIN_8;
     radar.scr.Init.SSD1306_Reset_Port = GPIO_1;
-    radar.scr.Init.SSD1306_Reset_Pin= GPIO_PIN_9;
+    radar.scr.Init.SSD1306_Reset_Pin = GPIO_PIN_9;
+    radar.scr.Init.SSD1306_CS_Port = GPIO_0;
+    radar.scr.Init.SSD1306_CS_Pin = GPIO_PIN_10;
 
-    radar.potentiometr_chanel = 1;
+    radar.potentiometr_chanel = 0;
 
     Init_Radar(&radar);
     Scan(&radar);
